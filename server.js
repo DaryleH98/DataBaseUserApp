e2668fdbcefaa842cda40ffe1cba8fb7a1baae5a
D4ryl3ne1 <Darylehen02@gmail.com>
2019-03-26 21:24:13 -0400

    js function that creates a table for sql database

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 const db =mysql.createConnection({
   host      :  'localhost',
   user      :  'me',
-  password  :  'Password',
-//  database  :  'usersmysql'
+  password  :  'Password123',
+  database  :  'usersmysql'
 });
 
 // Connect //Route Declaration
@@ -27,6 +27,17 @@ app.get('/createdb', (req, res) => {
     if(err) throw err;
     console.log(result);
     res.send ('Database created....');
+  });
+});
+
+//Create table
+app.get('/createpoststable', (req, res) => {
+  //Create query
+  let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY (id))';
+  db.query(sql, (err, result)=> {
+    if(err) throw err;
+    console.log(result);
+    res.send('Posts table created....');
   });
 });
 
