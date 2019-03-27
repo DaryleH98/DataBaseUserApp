1b4f95126f8c60978a421678b21bcd0f5b49f370
D4ryl3ne1 <Darylehen02@gmail.com>
2019-03-26 20:47:17 -0400

    Added server route declarations and a route that creates a database in js

--- server.js
+++ server.js
@@ -1,9 +1,41 @@
 //Express body parser
 const express = require('express'); //require function that loads the modules and give acess to their exports
-const bodyParser = require('body-parser');
-
-const store = require('./store');
+const mysql =  require('mysql');    //Require function that loads the module express
+const bodyParser = require('body-parser'); //For easy DOM manipulation
 const app = express();
+
+//Create connection
+const db =mysql.createConnection({
+  host      :  'localhost',
+  user      :  'me',
+  password  :  'Password',
+//  database  :  'usersmysql'
+});
+
+// Connect //Route Declaration
+db.connect((err) => {
+  if(err){
+    throw err;
+  }
+  console.log('MySql Connected.....');
+});
+
+//Create DB //Create a Route that creates a DataBase
+app.get('/createdb', (req, res) => {
+  let sql = 'CREATE DATABASE usersmysql';
+  db.query(sql, (err, result) => {
+    if(err) throw err;
+    console.log(result);
+    res.send ('Database created....');
+  });
+});
+
+const PORT = ('port', process.env.PORT || 3000);
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
+
+
 
 app.use(express.static(__dirname + '/public'));
 
@@ -17,7 +49,3 @@ app.post('/createUser', (req, res) => {
     .then(() => res.sendStatus(200))
 })
 
-const PORT = ('port', process.env.PORT || 3000);
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-});
